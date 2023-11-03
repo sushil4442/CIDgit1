eca7c7efa2f48c5ab3f74395cc0efe3ac5f2ea6d
Sushil.Gaikwad <Sushil.Gaikwad@DS025.step.com>
2023-11-03 16:09:26 +0530

    Login Commit & Added to suit

--- Test Suites/TSCID1.ts
+++ Test Suites/TSCID1.ts
@@ -46,4 +46,29 @@
       <testCaseId>Test Cases/TCID_5</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
+   <testCaseLink>
+      <guid>73ae4ff6-6c6e-4bda-b081-85369f325a74</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>544956d2-ce14-4fb2-bf93-5ebb99a902cb</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>0fc12a70-35f3-4d24-afc9-87bfcc6ffe3b</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>ac4f12fa-2a83-4c50-972b-c3c4fa5edd6f</variableId>
+      </variableLink>
+   </testCaseLink>
 </TestSuiteEntity>
