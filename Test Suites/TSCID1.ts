a917f0fa43cbafb107f1c649f8fece82ab9abf2d
Sushil.Gaikwad <Sushil.Gaikwad@DS025.step.com>
2023-11-06 12:45:48 +0530

    Only Login flow commited

--- Test Suites/TSCID1.ts
+++ Test Suites/TSCID1.ts
@@ -12,41 +12,6 @@
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>877fae5b-438d-4a24-b6ac-3bda6d10bb1e</testSuiteGuid>
    <testCaseLink>
-      <guid>56be27ee-e844-4af9-8672-49a26a7e11ea</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/TCID_1</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>db1707e0-ef8d-4a52-862b-dfcadecf3c44</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/TCID_2</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>b0996414-7f40-4927-957f-aad302cb8810</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/TCID_3</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>bbee7eef-3269-4866-8207-ffc207fb5a54</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/TCID_4</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>2d6c09e5-6ceb-4743-9440-56dac0f89dbd</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/TCID_5</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
       <guid>73ae4ff6-6c6e-4bda-b081-85369f325a74</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
