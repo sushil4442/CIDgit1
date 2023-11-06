0f8845bcd8be1569279f6f885883c80959c26990
Sushil.Gaikwad <Sushil.Gaikwad@DS025.step.com>
2023-11-06 17:22:42 +0530

    Start Process Commited

--- Scripts/Login/Script1699001747728.groovy
+++ Scripts/Login/Script1699001747728.groovy
@@ -17,6 +17,8 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
+println("Starts Process")
+
 WebUI.openBrowser(str_URL)
 
 WebUI.maximizeWindow()
